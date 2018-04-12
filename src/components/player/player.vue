b7f5da93ba2eefdf91c40381f599e18536089933
sacter <741170471@qq.comm>
2018-04-12 20:59:23 +0800

    other

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -317,7 +317,7 @@
       handleLyric({lineNum, txt}) {
         this.currentLineNum = lineNum;
         if (lineNum > 5) {
-          let lineEl = this.$refs.lyricLine[lineNum - 5]
+          let lineEl = this.$refs.lyricLine[lineNum - 5];
           this.$refs.lyricList.scrollToElement(lineEl, 1000)
         } else {
           this.$refs.lyricList.scrollTo(0, 0, 1000)
