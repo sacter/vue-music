021673abb6fb14e9bb0925f20a12a45294408dae
sacter <741170471@qq.com>
2018-04-29 20:41:08 +0800

    song-list

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -2,10 +2,13 @@
   <div class="song-list">
     <ul>
         <li @click="selectItem(song,index)" v-for="(song,index) in songs" :key="song.id" class="item">
-            <div class="content">
-                <h2 class="name">{{song.name}}</h2>
-                <p class="desc">{{getDesc(song)}}</p>
-            </div>
+          <div class="rank" v-show="rank">
+            <span :class="getRankCls(index)" v-text="getRankText(index)"></span>
+          </div>
+          <div class="content">
+              <h2 class="name">{{song.name}}</h2>
+              <p class="desc">{{getDesc(song)}}</p>
+          </div>
         </li>
     </ul>
   </div>
@@ -27,9 +30,21 @@
           selectItem(item,index){
             this.$emit('select',item,index);
           },
-            getDesc(song){
-                return `${song.singer} . ${song.album}`;             
+          getDesc(song){
+              return `${song.singer} . ${song.album}`;             
+          },
+          getRankCls(index) {
+            if (index <= 2) {
+              return `icon icon${index}`
+            } else {
+              return 'text'
             }
+          },
+          getRankText(index) {
+            if (index > 2) {
+              return index + 1
+            }
+          }
         }
     }
 </script>
