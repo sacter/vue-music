9ff3d2311b35fcb86d6dbc202a642cc3b5e8db0c
sacter <741170471@qq.com>
2018-05-09 21:19:56 +0800

    search

--- src/components/suggest/suggest.vue
+++ src/components/suggest/suggest.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="suggest">
     <ul class="suggest-list">
-      <li class="suggest-item">
+      <li class="suggest-item" v-for="item in result" :key="item.id">
         <div class="icon">
-          <i></i>
+          <i :class="getIconCls(item)"></i>
         </div>
         <div class="name">
-          <p class="text"></p>
+          <p class="text" v-html="getDisplayName(item)"></p>
         </div>
       </li>
       <!-- <loading v-show="hasMore" title=""></loading> -->
@@ -18,22 +18,85 @@
   // import Scroll from 'base/scroll/scroll'
   // import Loading from 'base/loading/loading'
   // import NoResult from 'base/no-result/no-result'
-  // import {search} from 'api/search'
-  // import {ERR_OK} from 'api/config'
-  // import {createSong} from 'common/js/song'
+  import {search} from 'api/search'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
   // import {mapMutations, mapActions} from 'vuex'
   // import Singer from 'common/js/singer'
+
+  const TYPE_SINGER = 'singer'
+  const perpage = 20
 
   export default {
     props: {
       query: {
         type: String,
         default: ''
+      },
+      showSinger: {
+        type: Boolean,
+        default: true
+      }
+    },
+    data() {
+      return {
+        page: 1,
+        pullup: true,
+        beforeScroll: true,
+        hasMore: true,
+        result: []
       }
     },
     methods: {
       search () {
-        
+        this.page = 1
+        // this.hasMore = true
+        // this.$refs.suggest.scrollTo(0, 0)
+        search(this.query, this.page, this.showSinger, perpage).then((res) => {
+          if (res.code === ERR_OK) {
+            this.result = this._genResult(res.data)
+            // this._checkMore(res.data)
+          }
+        })
+      },
+      getDisplayName(item) {
+        if (item.type === TYPE_SINGER) {
+          return item.singername
+        } else {
+          return `${item.name}-${item.singer}`
+        }
+      },
+      getIconCls(item) {
+        if (item.type === TYPE_SINGER) {
+          return 'icon-mine'
+        } else {
+          return 'icon-music'
+        }
+      },
+      _genResult(data) {
+        let ret = []
+        if (data.zhida && data.zhida.singerid) {
+          ret.push({...data.zhida, ...{type: TYPE_SINGER}})
+        }
+        if (data.song) {
+          ret = ret.concat(this._normalizeSongs(data.song.list))
+        }
+        return ret
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((musicData) => {
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      },
+      _checkMore(data) {
+        const song = data.song
+        if (!song.list.length || (song.curnum + song.curpage * perpage) > song.totalnum) {
+          this.hasMore = false
+        }
       }
     },
     watch: {
