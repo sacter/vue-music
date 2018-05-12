be23a3c05f77a3115c4106df71255272cf7e035f
sacter <741170471@qq.com>
2018-05-12 20:45:41 +0800

    suggest action

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,6 +2,12 @@ import * as types from './mutation-types'
 import { playMode } from 'common/js/config'
 import { shuffle } from 'common/js/util'
 
+function findIndex(list, song) {
+    return list.findIndex((item) => {
+        return item.id === song.id
+    })
+}
+
 export const selectPlay = function({ commit, state }, { list, index }) {
     commit(types.SET_SEQUENCE_LIST, list)
     commit(types.SET_PLAYLIST, list)
